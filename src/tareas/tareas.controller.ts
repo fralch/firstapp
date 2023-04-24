7e7e0157d9fa4bf27ef84dcb7e45f0b7e840d555
Frank Cairampoma <ingfralch@gmail.com>
2023-04-24 11:19:35 -0500

    ya esta, falta validar

--- src/tareas/tareas.controller.ts
+++ src/tareas/tareas.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Post, Delete, Param, Patch } from '@nestjs/common';
 import { TareasService } from './tareas.service';
+import { EstadoTarea} from './tarea.entity';
 
 @Controller('tareas') // http://localhost:3000/tareas (GET, POST, PUT, DELETE)
 export class TareasController {
@@ -19,7 +20,7 @@ export class TareasController {
     return this.tareasService.deleteTarea(id);
   }
   @Patch(":id")
-  updateTarea(@Param("id") id: string, @Body() filas: any) {
+  updateTarea(@Param("id") id: string, @Body() filas: {titulo?: string, descripcion?: string, estado?: EstadoTarea}) {
     return this.tareasService.updateTarea(id, filas);
   } 
 }
